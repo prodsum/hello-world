f3311ac084aa8a7ae3d315c3e9a0f015c2d5a9ee
adso <psonic@poczta.onet.pl>
2016-05-08 16:28:51 +0200

    add string.c

---
+++ string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int my_strlen(const char *s)
+{
+	const char *p = s;
+	while (*p)
+		++p;
+	return (p - s);
+}
+
+int main(void)
+{
+	int i;
+	const char *s[] = 
+	{
+	"Git tutorials",
+	"Tutorials Point"
+	};
+	for (i = 0; i < 2; ++i)
+		printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+	return 0;
+}
